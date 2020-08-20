02bb22bd7cb12c1ff96975a6c9c621e10bed07f5
Etai Barash <etai.bar3@gmail.com>
2020-08-20 12:31:36 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  // const [isSession, setSession] = useState(true);
+  // const [timerSecond, setTimerSecond] = useState(0);
+  // const [intervalid, setIntervalid] = useState(0);
+  const [sessionDuration, setSessionDuration] = useState(25);
+  const [breakDuration, setBreakDuration] = useState(5);
+  const [timerMinute, setTimerMinute] = useState(5);
+  const [isPlay, setPlay] = useState(true);
+
+  const increaseSession = () => {
+    setSessionDuration((prevState) => {
+      return {
+        sessionDuration: prevState.sessionDuration + 1,
+        timerMinute: prevState.sessionDuration + 1,
+      };
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pomodoro Timer</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
